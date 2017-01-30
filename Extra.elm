b7855a24815adb99ff6ead69a5ebc0b0d0f3eb6f
chaseries <hello@chaseries.com>
2017-01-30 02:30:59 -0500

    Add Python-like conveniences
    The minor conveniences affored by Python's string module are hard to
    forget about when using Elm's String. I've added them here, hopefully
    without overloading the library with cruft.
    
    I derive their names from Python's with a little added Elm twist.

--- Extra.elm
+++ Extra.elm
@@ -1,7 +1,36 @@
-module Extra exposing (..)
+module Extra exposing 
+  ( asciiLetters, digits, punctuation, isAlpha, isNumeric, isAlphaNumeric 
+  , head, tail, containsAny, excludes, excludesAll
+  , capitalize, title
+  )
 
 import String
 import Char
+
+
+asciiLetters : String
+asciiLetters =
+  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+punctuation : String
+punctuation =
+  "!\"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~"
+
+digits : String
+digits =
+  "0123456789"
+
+isAlpha : String -> Bool
+isAlpha str =
+  isOnlyOf str asciiLetters
+
+isNumeric : String -> Bool
+isNumeric str =
+  isOnlyOf str digits
+
+isAlphaNumeric : String -> Bool
+isAlphaNumeric str =
+  isOnlyOf str (asciiLetters ++ digits)
 
 head : String -> String
 head string =
@@ -10,6 +39,26 @@ head string =
 tail : String -> String
 tail string =
   String.dropLeft 1 string
+
+containsAny : String -> String -> Bool
+containsAny sub str =
+  case (String.uncons sub) of
+    Just (head, tail) ->
+      case String.contains (String.fromChar head) str of
+        False -> containsAny tail str
+        True -> True
+    Nothing ->
+      False
+
+excludes : String -> String -> Bool
+excludes sub str =
+  String.contains sub str
+    |> not
+
+excludesAll : String -> String -> Bool
+excludesAll sub str =
+  containsAny sub str
+    |> not
 
 capitalize : String -> String
 capitalize string =
@@ -29,3 +78,12 @@ title string =
     |> List.map capitalize
     |> String.join " "
 
+isOnlyOf : String -> String -> Bool
+isOnlyOf str charset =
+  case (String.uncons str) of
+    Just (head, tail) ->
+      case String.contains (String.fromChar head) charset of
+        True -> isOnlyOf tail charset
+        False -> False
+    Nothing ->
+      True
